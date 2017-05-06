758bc1f2afc0af5ce62f2c82476b9c6b51e74733
Musab36 <musabhd@gmail.com>
2017-05-06 18:37:56 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
+  $("#game").submit(function(event) {
+    $("ul").empty();
+    var number = parseInt($("#mynum").val());
 
+    for (x = 1; x <= number; x++)
+      if (x % 3 === 0 && x % 5 === 0) {
+        $("ul").append("<li>pingpong</li>");
+
+      } else if (x % 3 === 0) {
+      $("ul").append("<li>ping</li>");
+
+    } else if (x % 5 === 0) {
+      $("ul").append("<li>pong</li>");
+
+    } else {
+      $("ul").append("<li>" + x + "</li>");
+    }
+    event.preventDefault();
+  });
+});
